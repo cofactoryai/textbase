db552ce54acb8a495bfcbba20ed3488bddb79c60
Electronica-dev <srsamartha@gmail.com>
2023-07-26 23:17:51 +0530

    renamed process variable

--- textbase/textbase_cli.py
+++ textbase/textbase_cli.py
@@ -20,16 +20,16 @@ def cli():
 @cli.command()
 @click.argument('filename', type=click.Path(exists=True))
 def test(filename):
-    p = None  # Declare p outside the try block to ensure access in the finally block
+    process = None  # Declare process outside the try block to ensure access in the finally block
     try:
         # Get the directory containing the file and add it to the Python path
         file_directory = os.path.dirname(filename)
         sys.path.append(file_directory)
-        
+
         my_env = os.environ.copy()
         my_env['FILE_PATH'] = filename
         logging.info(my_env['FILE_PATH'])
-        p = subprocess.Popen(['uvicorn', 'textbase.backend:app', '--reload', '--host', '0.0.0.0', '--port', '4000'], env=my_env)
+        process = subprocess.Popen(['uvicorn', 'textbase.backend:app', '--reload', '--host', '0.0.0.0', '--port', '4000'], env=my_env)
 
         # Import the module containing the decorated function
         module_name = os.path.basename(filename)[:-3] if filename.endswith('.py') else os.path.basename(filename)
@@ -37,22 +37,22 @@ def test(filename):
 
         if hasattr(module, 'on_message'):
             print("Chatbot is running. Visit http://localhost:4000/ in your web browser to interact.")
-            p.wait()
+            process.wait()
         else:
             print("Error: 'on_message' function not found in the file.")
-    
+
     except Exception as e:
         # Log the exception or print a custom error message
         print(f"Error occurred: {e}")
         sys.exit(1)
-    
+
     finally:
         # Ensure subprocess is terminated when script exits
-        if p:
-            p.terminate()  # Try terminating the process first
+        if process:
+            process.terminate()  # Try terminating the process first
             time.sleep(1)  # Add a short delay before killing the process
-            p.kill()  # Kill the process if it did not terminate gracefully
-        
+            process.kill()  # Kill the process if it did not terminate gracefully
+
 def deploy():
     pass
 
