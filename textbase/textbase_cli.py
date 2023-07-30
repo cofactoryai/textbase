cdd46446d523072b1b2275ecf35467f61a2fa27c
Silas Alberti <sulm.silas@gmail.com>
2023-07-29 16:39:29 -1000

    Remove host 0.0.0.0

--- textbase/textbase_cli.py
+++ textbase/textbase_cli.py
@@ -4,7 +4,8 @@ import click
 import importlib.util
 import subprocess
 import logging
-import time  
+import time
+
 # from textbase.download import download_and_extract_zip
 
 logging.basicConfig(level=logging.INFO)
@@ -15,17 +16,19 @@ logging.basicConfig(level=logging.INFO)
 # destination_folder = os.path.join(os.getcwd(), "textbase")
 # download_and_extract_zip(zip_url, destination_folder)
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
-@click.argument('filename', type=click.Path(exists=True))
+@click.argument("filename", type=click.Path(exists=True))
 def test(filename):
     """
     The `test` function runs a Python script as a subprocess and terminates it gracefully when the
     script finishes or encounters an error.
-    
+
     :param filename: The `filename` parameter is the path to a file that will be used in the script. It
     is expected to be an existing file
     """
@@ -34,38 +37,49 @@ def test(filename):
         # Get the directory containing the file and add it to the Python path
         file_directory = os.path.dirname(filename)
         sys.path.append(file_directory)
-        
+
         my_env = os.environ.copy()
-        my_env['FILE_PATH'] = filename
-        logging.info(my_env['FILE_PATH'])
-        p = subprocess.Popen(['uvicorn', 'textbase.backend:app', '--reload', '--host', '0.0.0.0', '--port', '4000'], env=my_env)
+        my_env["FILE_PATH"] = filename
+        logging.info(my_env["FILE_PATH"])
+        p = subprocess.Popen(
+            ["uvicorn", "textbase.backend:app", "--reload", "--port", "4000"],
+            env=my_env,
+        )
 
         # Import the module containing the decorated function
-        module_name = os.path.basename(filename)[:-3] if filename.endswith('.py') else os.path.basename(filename)
+        module_name = (
+            os.path.basename(filename)[:-3]
+            if filename.endswith(".py")
+            else os.path.basename(filename)
+        )
         module = importlib.import_module(module_name)
 
-        if hasattr(module, 'on_message'):
-            print("Chatbot is running. Visit http://localhost:4000/ in your web browser to interact.")
+        if hasattr(module, "on_message"):
+            print(
+                "Chatbot is running. Visit http://localhost:4000/ in your web browser to interact."
+            )
             p.wait()
         else:
             print("Error: 'on_message' function not found in the file.")
-    
+
     except Exception as e:
         # Log the exception or print a custom error message
         print(f"Error occurred: {e}")
         sys.exit(1)
-    
+
     finally:
         # Ensure subprocess is terminated when script exits
         if p:
             p.terminate()  # Try terminating the process first
             time.sleep(1)  # Add a short delay before killing the process
             p.kill()  # Kill the process if it did not terminate gracefully
-        
+
+
 def deploy():
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         cli()
     except KeyboardInterrupt:
