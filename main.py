b62c3538a74478836999811c69ef23238ddce810
SP-011 <psairamk0000@gmail.com>
2023-07-31 02:52:16 +0530

    Created a Interview helper chatbot

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ import os
 from typing import List
 
 # Load your OpenAI API key
-models.OpenAI.api_key = "YOUR_API_KEY"
+models.OpenAI.api_key = "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"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 # Prompt for GPT-3.5 Turbo
-SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
-"""
-
+# SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
+SYSTEM_PROMPT = """You are chatting with a Job Interview Preparation Chatbot. \
+        First greet student with offering your service, then proceed.\
+        This chatbot is designed to help you improve your interview skills and provide tips to boost your confidence. \
+    Keep in mind that while the chatbot can offer valuable advice, practice and preparation are essential for a successful interview.\
+        Please remember that the chatbot cannot predict specific interview questions or guarantee \
+    the outcome of your interviews. However, it can provide general guidance and best practices.\
+        When student tells the domain of interview then ask him that does he need mock interview or general tips.\
+        If user chooses to mock interview then ask 5-10 questions about the domain provided and in the end provide a \
+    feedback on students answers.\
+    """
 
 @textbase.chatbot("talking-bot")
 def on_message(message_history: List[Message], state: dict = None):
@@ -23,16 +31,26 @@ def on_message(message_history: List[Message], state: dict = None):
     Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
     """
 
+     # Extract the latest user message
+    user_input = message_history[-1].content.strip().lower()
+
     if state is None or "counter" not in state:
         state = {"counter": 0}
     else:
         state["counter"] += 1
 
-    # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history,
-        model="gpt-3.5-turbo",
-    )
+
+    # Function Calling: Support specific commands
+    if user_input == "help":
+        bot_response = "Sure! You can ask me anything, and I'll do my best to assist you."
+    elif user_input in ["interview tips", "tips"]:
+        bot_response = "To prepare for your job interview, research the company, practice technical questions, and work on your communication skills."
+    else:
+        # # Generate GPT-3.5 Turbo response
+        bot_response = models.OpenAI.generate(
+            system_prompt=SYSTEM_PROMPT,
+            message_history=message_history,
+            model="gpt-3.5-turbo",
+        )
 
     return bot_response, state
