a635e8fe84350e2fed546e43ea706f6398d2c718
MANISH KUMAR CHINTHA <chintamanishkumar@gmail.com>
2023-08-13 14:08:19 +0530

    Integrated WhatsApp and SMS - twilio

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from textbase.message import Message
 from textbase import models
 import os
 from typing import List
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request
 
 # Load your OpenAI API key
 models.OpenAI.api_key = "YOUR_API_KEY"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
+# Twilio credentials
+TWILIO_ACCOUNT_SID = "YOUR_TWILIO_ACCOUNT_SID"
+TWILIO_AUTH_TOKEN = "YOUR_TWILIO_AUTH_TOKEN"
+TWILIO_PHONE_NUMBER = "YOUR_TWILIO_PHONE_NUMBER"  # Your Twilio phone number
+
+app = Flask(__name__)
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
@@ -36,3 +45,26 @@ def on_message(message_history: List[Message], state: dict = None):
     )
 
     return bot_response, state
+
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    incoming_message = request.form.get('Body', '').lower()
+    response, state = on_message([Message(content=incoming_message)], state=None)
+
+    twilio_response = MessagingResponse()
+    twilio_response.message(response)
+
+    return str(twilio_response)
+
+@app.route('/sms', methods=['POST'])
+def handle_sms():
+    incoming_message = request.form.get('Body', '').lower()
+    response, state = on_message([Message(content=incoming_message)], state=None)
+
+    twilio_response = MessagingResponse()
+    twilio_response.message(response)
+
+    return str(twilio_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
