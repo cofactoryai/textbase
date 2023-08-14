ccabe276d6afe927331847d175d4679a58762df2
1VIP1786 <vipulpatil1786@gmail.com>
2023-08-14 21:16:48 +0530

    changes to codebase

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import List
 import difflib
 
 # Load your OpenAI API key
-models.OpenAI.api_key = "ENTER YOUR OPEN AI API_KEY HERE"
+models.OpenAI.api_key =""
 
 # Read health keywords from the file
 with open("health_keywords.txt", "r") as file:
@@ -34,7 +34,7 @@ def on_message(message_history: List[Message], state: dict = None):
         system_prompt += "User: " + " ".join(user_messages) + " "
 
     # If the input is a common greeting, provide a prompt to ask something related to healthcare
-    if any(greeting in user_input for greeting in ["hello", "hi", "hey", "how are you","suffering"]):
+    if any(greeting in user_input for greeting in ["hello", "hi", "hey", "how are you", "suffering"]):
         bot_response = "Hello! Feel free to ask me any health-related questions or share your healthcare concerns."
     elif is_health_related:
         response = models.OpenAI.generate(
@@ -68,5 +68,31 @@ def create_user_profile(name, contact_details, medical_history):
         "medical_history": medical_history
     }
 
+def interact_with_chatbot():
+    name = input("Enter your name: ")
+    contact_details = input("Enter your contact details: ")
+    medical_history = input("Enter your medical history: ")
+    create_user_profile(name, contact_details, medical_history)
+
+    while True:
+        user_input = input("You: ")
+        is_health_related = health_care_plugin(user_input)
+
+        if any(greeting in user_input for greeting in ["hello", "hi", "hey", "how are you"]):
+            bot_response = "Hello! Feel free to ask me any health-related questions or share your healthcare concerns."
+        elif is_health_related:
+            response = models.OpenAI.generate(
+                system_prompt="You are chatting with a Health Advisor. User: " + user_input,
+                message_history=[Message(content=user_input, role="user")],
+                model="gpt-3.5-turbo",
+                temperature=0.7,
+                max_tokens=100,
+            )
+            bot_response = response.choices[0].text.strip()
+        else:
+            bot_response = "I'm sorry, I can't help you with that. Please try asking me a health-related question."
+
+        print("Bot:", bot_response)
+
 if __name__ == "__main__":
-    textbase.run(debug=True)
+    interact_with_chatbot()
