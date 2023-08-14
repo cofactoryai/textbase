0449905a801072b89f0fa1be4a124046fbe1c858
ARVpiyush <PiyushL@arv.co.th>
2023-08-14 14:54:14 +0530

    voice chatbot

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
+import pyttsx3
+import speech_recognition as sr
 import textbase
 from textbase.message import Message
 from textbase import models
-
-# hiii
 import os
 from typing import List
 
-# Load your OpenAI API keygdtedt
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
+engine.setProperty('voices',voices[0].id)
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+# AI introduction at the start
+def intro():
+    introduction = "Hello, My name is Friday, I am your virtual host"
+    speak(introduction)
+    ask = "How can I help you, you can ask me anyting"
+    speak(ask)
+
+# Take continous input In every 5 seconds 
+def takeCommand():
+    print("please say after 5 second:::::::")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        query = r.recognize_google(audio,language='en-in')
+
+    except Exception as e:
+        s = "can you say that again please i could not here..."
+        speak(s)
+        return "none"
+    return query
+
+
+
+# Load your OpenAI API key
 models.OpenAI.api_key = "YOUR_API_KEY"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
@@ -30,11 +68,21 @@ def on_message(message_history: List[Message], state: dict = None):
     else:
         state["counter"] += 1
 
-    # # Generate GPT-3.5 Turbo response
+# # Generate GPT-3.5 Turbo response
     bot_response = models.OpenAI.generate(
         system_prompt=SYSTEM_PROMPT,
         message_history=message_history,
         model="gpt-3.5-turbo",
     )
 
+    speak(bot_response)
+
     return bot_response, state
+
+
+
+if __name__ == '__main__': 
+    intro()
+    while True:
+        query = takeCommand().lower()
+        on_message([query])
