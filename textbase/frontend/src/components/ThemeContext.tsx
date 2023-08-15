997f67b819abc8d5a86951f581de17ad055a650b
Harsh Singh <81004813+HarshS1611@users.noreply.github.com>
2023-08-15 12:42:06 +0530

    features added: customization panel, mulitmedia support, voice input (#39)
    * features added: customization panel, mulitmedia support, voice input
    
    * features updated: gradient color background and custom picture background
    
    * errors fixed
    
    * features added : added the dark theme option beside customization panel

---
+++ textbase/frontend/src/components/ThemeContext.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface Theme {
+  backgroundColor: string;
+  foregroundColor: string;
+  fontFamily: string;
+  avatar: string;
+  backgroundImage?: string;
+  gradientStart?: string;
+  gradientEnd?: string;
+}
+
+const defaultTheme: Theme = {
+  backgroundColor: '#ffffff',
+  foregroundColor: 'grey',
+  fontFamily: 'Arial, sans-serif',
+  avatar: '',
+  backgroundImage: '',
+  gradientStart: '#ffffff', 
+  gradientEnd: 'grey',
+};
+
+const ThemeContext = createContext<{
+  theme: Theme;
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+}>({
+  theme: defaultTheme,
+  setTheme: () => {},
+});
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
