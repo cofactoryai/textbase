423b5ec51d8511e6805bba0c086c68256d5f9f16
Maria Khan <memaria210@gmail.com>
2023-08-15 10:36:07 +0530

    moved main.py to examples/langchain_huggingface
    moved main.py to examples/langchain_huggingface and restored the previous main.py

--- examples/langchain_huggingface/main.py
+++ examples/langchain_huggingface/main.py
@@ -3,16 +3,17 @@ from textbase.message import Message
 from textbase import models
 import os
 from typing import List
+from dotenv import load_dotenv
 
+load_dotenv()
 # Load your OpenAI API key
-models.OpenAI.api_key = "YOUR_API_KEY"
+# models.OpenAI.api_key = "YOUR_API_KEY"
 # or from environment variable:
-# models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+models.HuggingFaceHub.api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
 """
-
 
 @textbase.chatbot("talking-bot")
 def on_message(message_history: List[Message], state: dict = None):
@@ -22,17 +23,18 @@ def on_message(message_history: List[Message], state: dict = None):
 
     Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
     """
-
+    
     if state is None or "counter" not in state:
         state = {"counter": 0}
     else:
         state["counter"] += 1
-
-    # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
+    #Generate Hugging face model response using Langchain
+    bot_response_new = models.LangchainHuggingFace.generate(
         message_history=message_history,
-        model="gpt-3.5-turbo",
+        system_prompt=SYSTEM_PROMPT,
+        model="google/flan-t5-xxl",
+        temperature=0.4, 
+        top_k=70
     )
-
-    return bot_response, state
+    
+    return bot_response_new, state
