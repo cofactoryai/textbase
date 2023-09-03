544eb7aa314a1e0eb04449f68493a08e337434cf
Pravee Singh <ratinsingh23@gmail.com>
2023-09-03 22:14:06 +0530

    cohere bot for response generation of given prompt added in examples

---
+++ examples/cohere-prompt-bot/main.py
@@ -0,0 +1,43 @@
+from textbase import bot, Message
+from textbase.models import Cohere
+from typing import List
+
+# Load your Cohere API key
+Cohere.api_key = "JRqiMRt2MtCqje9DDEYQd3ZIUEDxLdOfwkG9cem2"
+
+# Prompt for `command` (cohere's model)
+# you add your sample prompts here
+SYSTEM_PROMPT = """how python interpreter works!
+"""
+
+@bot()
+def on_message(message_history: List[Message], state: dict = None):
+
+    # Generate `command` (cohere's model) response
+    bot_response = Cohere.generate(
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history, # Assuming history is the list of user messages
+        model="command",
+    )
+
+    response = {
+        "data": {
+            "messages": [
+                {
+                    "data_type": "STRING",
+                    "value": bot_response
+                }
+            ],
+            "state": state
+        },
+        "errors": [
+            {
+                "message": ""
+            }
+        ]
+    }
+
+    return {
+        "status_code": 200,
+        "response": response
+    }
