7f6bd97705feba73c725f82800b11d6af61919e7
Prajjwal <prajju.s1999@gmail.com>
2023-09-03 22:30:32 +0530

    Added a feature to openai-bot for responding with image urls on prompts

--- examples/openai-bot/main.py
+++ examples/openai-bot/main.py
@@ -3,7 +3,7 @@ from textbase.models import OpenAI
 from typing import List
 
 # Load your OpenAI API key
-OpenAI.api_key = ""
+OpenAI.api_key = "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"
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like.
@@ -14,12 +14,25 @@ pleasant chat!
 @bot()
 def on_message(message_history: List[Message], state: dict = None):
 
+    prompt_check = ['photo','image','picture']
+    flag = 0
+    print(message_history)
     # Generate GPT-3.5 Turbo response
-    bot_response = OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history, # Assuming history is the list of user messages
-        model="gpt-3.5-turbo",
-    )
+    for prompt in prompt_check:
+        if prompt in message_history[len(message_history)-1]['content'][0]['value']:
+            flag = 1
+            bot_response = OpenAI.image_generate(
+            system_prompt=SYSTEM_PROMPT,
+            message_history=message_history, # Assuming history is the list of user messages
+            model="gpt-3.5-turbo",
+            )
+            break
+    if flag != 1:
+        bot_response = OpenAI.generate(
+            system_prompt=SYSTEM_PROMPT,
+            message_history=message_history, # Assuming history is the list of user messages
+            model="gpt-3.5-turbo",
+        )
 
     response = {
         "data": {
