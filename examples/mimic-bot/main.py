8dd313afe1709d2b97a1e1e45de2232d10009594
bharatchhabraai <141989430+bharatchhabraai@users.noreply.github.com>
2023-09-03 09:05:43 +0530

    Added space

--- examples/mimic-bot/main.py
+++ examples/mimic-bot/main.py
@@ -6,6 +6,7 @@ def on_message(message_history: List[Message], state: dict = None):
 
     # Mimic user's response
     bot_response = message_history[-1]["content"]
+
     # message_history[-1]["content"] structure is 
 
     # [
