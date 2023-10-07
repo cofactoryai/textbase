ed81b307ec9977dbbf24ef84da1e796d9a53dca8
sammyCofactory <samarthar@cofactory.ai>
2023-10-07 16:55:07 +0530

    Update Image class

--- textbase/classes.py
+++ textbase/classes.py
@@ -1,6 +1,15 @@
-class ImageUrl:
-    def __init__(self, url):
-        self.url = url
+from textbase.helpers import convert_img_to_url
+from PIL.Image import Image as PILImageClass
 
-def image(url):
-    return ImageUrl(url)
+class Image:
+    def __init__(self, url="", pil_image=""):
+        if url and pil_image:
+            raise TypeError("Only url OR pil_image can be given.")
+        if not isinstance(pil_image, PILImageClass):
+            raise TypeError("Not a valid PIL image.")
+
+        self.url = url
+        self.pil_image = pil_image
+
+    def upload_pil_to_bucket(self):
+        self.url = convert_img_to_url(pil_image=self.pil_image)
