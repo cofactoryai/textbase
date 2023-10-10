b7472398d87a5a4e48a7825b9c3e71becf248263
sammyCofactory <samarthar@cofactory.ai>
2023-10-10 18:40:27 +0530

    Add file_extension parameter

--- textbase/helpers.py
+++ textbase/helpers.py
@@ -9,14 +9,15 @@ def convert_img_to_url(image_file_path="", pil_image: PILImageClass=None) -> str
         # convert PIL object to a byte array
         img_byte_arr = BytesIO()
         img_format = pil_image.format
-        pil_image.save(img_byte_arr, format=img_format)
+        pil_image.save(img_byte_arr, img_format)
         img_byte_arr = img_byte_arr.getvalue()
 
         img_file = {
             'file': img_byte_arr
         }
         data = {
-            'parent_path': 'bot'
+            'parent_path': 'bot',
+            'file_extension': img_format.lower()
         }
 
         response = requests.post(URL, files=img_file, data=data)
@@ -25,17 +26,17 @@ def convert_img_to_url(image_file_path="", pil_image: PILImageClass=None) -> str
             return f'Error: {response.json()["error"]}'
         else:
             return response.json()['image_url']
-    with open(image_file_path, 'rb') as img_file:
-        img_file = {
-            'file': img_file
-        }
-        data = {
-            'parent_path': 'bot'
-        }
-
-        response = requests.post(URL, files=img_file, data=data)
-
-    if 'error' in response.json():
-        return f'Error: {response.json()["error"]}'
     else:
-        return response.json()['image_url']
+        with open(image_file_path, 'rb') as img_file:
+            img_file = {
+                'file': img_file
+            }
+            data = {
+                'parent_path': 'bot'
+            }
+            response = requests.post(URL, files=img_file, data=data)
+
+        if 'error' in response.json():
+            return f'Error: {response.json()["error"]}'
+        else:
+            return response.json()['image_url']
