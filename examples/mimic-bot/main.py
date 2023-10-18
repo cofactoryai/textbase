0d6222bb2d6fe4ddbb2048750e3ce76a3ee7a108
sammyCofactory <samarthar@cofactory.ai>
2023-10-18 12:32:55 +0530

    Update mimic bot logic

--- examples/mimic-bot/main.py
+++ examples/mimic-bot/main.py
@@ -1,12 +1,24 @@
 from typing import List
 from textbase import bot, Message
-
+from textbase.datatypes import Image, Video, Audio, File
 
 @bot()
 def on_message(message_history: List[Message], state: dict = None):
 
     # Mimic user's response
-    bot_response = [message["value"] for message in message_history[-1]["content"]]
+    bot_response = []
+    for message in message_history[-1]["content"]:
+        match message['data_type']:
+            case 'STRING':
+                bot_response.append(message['value'])
+            case 'IMAGE_URL':
+                bot_response.append(Image(message['value']))
+            case 'VIDEO_URL':
+                bot_response.append(Video(message['value']))
+            case 'AUDIO_URL':
+                bot_response.append(Audio(message['value']))
+            case 'FILE_URL':
+                bot_response.append(File(message['value']))
 
     # message_history[-1]["content"] structure is
 
